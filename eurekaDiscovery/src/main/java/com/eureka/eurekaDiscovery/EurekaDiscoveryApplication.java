011ce7e27ed6954ec9e8b4d97bb1776fc76a4c40
mouadhfarhat <fmouadh50@gmail.com>
2025-02-24 00:30:51 +0100

    EurekaDiscovery

---
+++ eurekaDiscovery/src/main/java/com/eureka/eurekaDiscovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.eurekaDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
