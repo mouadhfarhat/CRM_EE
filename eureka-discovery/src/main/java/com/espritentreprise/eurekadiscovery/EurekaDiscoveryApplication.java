ef42734033d4f8978a9e06f2ec6bb4a45d9c7d6d
mouadhfarhat <fmouadh50@gmail.com>
2025-02-25 03:17:54 +0100

    serviceFormation

--- eureka-discovery/src/main/java/com/espritentreprise/eurekadiscovery/EurekaDiscoveryApplication.java
+++ eureka-discovery/src/main/java/com/espritentreprise/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaDiscoveryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaDiscoveryApplication.class, args);
     }
--- formation-service/src/main/java/com/espritentreprise/formation_service/FormationServiceApplication.java
+++ formation-service/src/main/java/com/espritentreprise/formation_service/FormationServiceApplication.java
@@ -2,10 +2,8 @@ package com.espritentreprise.formation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class FormationServiceApplication {
 
     public static void main(String[] args) {
