1cf0260543d7dfc8ef6dc756ef3653ed43289600
mouadhfarhat <fmouadh50@gmail.com>
2025-04-03 02:27:29 +0100

    frontend

---
+++ frontend/AdminDashboard/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+export const routes: Routes = [
+     { path: 'login', component: LoginComponent },
+     { path: '', component: HomeComponent },
+       
+];
--- frontend/CrmFrontend/src/app/components/home/home.component.ts
+++ frontend/CrmFrontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormationComponent } from '../formation/formation.component';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ FormationComponent ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  standalone: true
 })
 export class HomeComponent {
 
