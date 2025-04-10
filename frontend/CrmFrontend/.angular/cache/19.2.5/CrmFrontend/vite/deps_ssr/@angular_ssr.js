f4c589aca60726fb986b0147119261ea48290856
mouadhfarhat <fmouadh50@gmail.com>
2025-04-10 13:28:58 +0100

    main

---
+++ frontend/CrmFrontend/.angular/cache/19.2.5/CrmFrontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,39 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-AEKAMVIF.js";
+import "./chunk-2AURD6F5.js";
+import "./chunk-I5MUSD6H.js";
+import "./chunk-FPG3BCQW.js";
+import "./chunk-IXIDN2OZ.js";
+import "./chunk-4RDLTH23.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
---
+++ frontend/CrmFrontend/.angular/cache/19.2.5/CrmFrontend/vite/deps_ssr/primeng_api.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-ROM3AULP.js";
+import "./chunk-IXIDN2OZ.js";
+import "./chunk-4RDLTH23.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
