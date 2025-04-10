927406bd69aea6265519a178774ca0f943d27f6c
mouadhfarhat <fmouadh50@gmail.com>
2025-04-10 11:32:53 +0100

    commit

---
+++ frontend/AdminDashboard/.angular/cache/19.2.6/AdminDashboard/vite/deps/primeng_api.js
@@ -0,0 +1,37 @@
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-GDMU3SSR.js";
+import "./chunk-CY4MWE6Z.js";
+import "./chunk-DMGSTSJH.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
